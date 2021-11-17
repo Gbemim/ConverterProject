838b1cb191daaebb000630c84cc810f273cd8c8a
Dom <Dom@Dom>
2021-11-17 14:19:04 -0500

    removed setlocation function, not needed

--- ConverterProject/src/view/ConverterPanel.java
+++ ConverterProject/src/view/ConverterPanel.java
@@ -7,26 +7,20 @@ import java.awt.*;
 
 public class ConverterPanel extends JPanel {
 	JTextArea greenArea = new JTextArea("0",14,21);
-	JTextArea yellowArea = new JTextArea("0",14,21);
 	JTextArea orangeArea = new JTextArea("0",14,21);
+	JTextArea yellowArea = new JTextArea("0",14,21);
 	
 	public ConverterPanel() {
 		setTextColors();
-		setTextPositions();
 		add(greenArea);
-		add(yellowArea);
 		add(orangeArea);
+		add(yellowArea);
+
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-	}
-	
-	public void setTextPositions() {
-		greenArea.setLocation(100,10);
-		yellowArea.setLocation(305,10);
-		orangeArea.setLocation(200,215);
 	}
 	
 	public void setTextColors() {
