4dce43365c8d30b7c8b9a3eadc407c1b252c7573
Dom <Dom@Dom>
2021-11-17 13:44:00 -0500

    created JTextAreas, set position, and set color functions.

--- ConverterProject/src/view/ConverterPanel.java
+++ ConverterProject/src/view/ConverterPanel.java
@@ -2,14 +2,17 @@ package view;
 
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+
 import java.awt.*;
 
 public class ConverterPanel extends JPanel {
-	JTextArea greenArea = new JTextArea(200,200);
-	JTextArea yellowArea = new JTextArea(200,200);
-	JTextArea orangeArea = new JTextArea(200,200);
+	JTextArea greenArea = new JTextArea("0",15,22);
+	JTextArea yellowArea = new JTextArea("0",15,22);
+	JTextArea orangeArea = new JTextArea("0",15,22);
 	
 	public ConverterPanel() {
+		setTextColors();
+		setTextPositions();
 		add(greenArea);
 		add(yellowArea);
 		add(orangeArea);
@@ -18,20 +21,18 @@ public class ConverterPanel extends JPanel {
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		g.setColor(Color.GREEN);
-		g.fillRect(100, 10, 200, 200);
-		
-		g.setColor(Color.ORANGE);
-		g.fillRect(305, 10, 200, 200);
-		
-		g.setColor(Color.YELLOW);
-		g.fillRect(200, 215, 200, 200);
-		
-		//paintText(g);
 	}
 	
-	public void textAreaPositions() {
+	public void setTextPositions() {
+		greenArea.setLocation(100,10);
+		yellowArea.setLocation(305,10);
+		orangeArea.setLocation(200,215);
+	}
+	
+	public void setTextColors() {
+		greenArea.setBackground(Color.GREEN);
+		yellowArea.setBackground(Color.YELLOW);
+		orangeArea.setBackground(Color.ORANGE);
 	}
 	
 }
